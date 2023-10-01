ea95667bf7836db8b01bfe628dcea4db2ca4ef2f
takeru227 <takenosuke227@gmail.com>
2023-10-01 17:17:29 +0900

    サンプルデータの修正

--- weather_learn.py
+++ weather_learn.py
@@ -1,30 +1,48 @@
-import numpy as np
-from sklearn import svm
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 # csvファイルの読み込み
-npArray = np.loadtxt("data_fuk.csv", delimiter=",", dtype="float", encoding="utf-8")
+df = pd.read_csv("data_fuk.csv", delimiter=",", dtype="float")
 
-# 説明変数の格納
-x = npArray[:, 0:4]
+# サンプルデータの用意
+data = {
+    "平均気温": [15, 18, 20, 22, 25],
+    "降水量": [0, 20, 5, 1, 0],
+    "日照時間": [5, 3, 8, 10, 12],
+    "平均湿度": [60, 75, 50, 30, 25],
+    "平均雲量": [30, 60, 20, 10, 5],
+}
+df = pd.DataFrame(data)
 
-# 目的変数の格納
-y = npArray[:, 4:5].ravel()
+# 特徴量と目的変数に分割
+X = df.drop("平均雲量", axis=1)
+y = df["平均雲量"]
 
-# 学習手法にSVMを選択
-model = svm.SVC()
+# データを訓練データとテストデータに分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# 学習
-model.fit(x, y)
+# モデルの選択と訓練（線形回帰モデルを使用）
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
 
-# 評価データ(ここは自分で好きな値を入力)
-weather = [[9, 0, 7.9, 6.5]]
+# 新しいデータの作成
+new_data = pd.DataFrame({"平均気温": [23], "降水量": [5], "日照時間": [9], "平均湿度": [45]})
 
-# predict関数で、評価データの天気を予測
-ans = model.predict(weather)
+# 新しいデータに対する予測
+predicted_cloud_cover = model.predict(new_data)[0]
+print(f"Predicted Average Cloud Cover for New Data: {predicted_cloud_cover}")
 
-if ans >= 0:
-    print("晴れです")
-elif ans > 2:
-    print("曇りです")
-elif ans > 8:
-    print("雨です")
+# カテゴリ出力の処理
+if predicted_cloud_cover <= 1:
+    print("快晴")
+elif 2 <= predicted_cloud_cover <= 8:
+    print("晴れ")
+else:
+    print("曇り")
+
+# モデルの保存
+with open("linear_regression_model.pkl", "wb") as file:
+    pickle.dump(model, file)
